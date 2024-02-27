0702b2c5ce856804460c15e0dc77b8e5266e6984
Brian Chebon <brianlchebon@gmail.com>
2024-02-27 15:19:53 +0300

    added var needed

--- main.go
+++ main.go
@@ -7,18 +7,27 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Printf("conferenceName is %T, remainingTickets is %T, conferenceTickets is %T\n", conferenceName, remainingTickets, conferenceTickets)
-
 	fmt.Printf("Welcome to %v Booking Application\n", conferenceName)
 	fmt.Printf("We Have total of %v Tickets and %v are still remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get Your Ticket here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 	//ask user for their name
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email address:")
+	fmt.Scan(&email)
+	fmt.Println("Enter Number of tickets you need:")
+	fmt.Scan(&userTickets)
 
-	userName = "Brian"
-	userTickets = 50
-	fmt.Printf("User %v Booked %v Tickets\n", userName, userTickets)
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("User %v %v with email %v Booked %v Tickets\n", firstName, lastName, email, userTickets)
+	fmt.Printf("Remaining tickets are: %v\n", remainingTickets)
 
 }
