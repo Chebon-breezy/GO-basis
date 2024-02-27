d746966a0fb2e213a2ca29f8795e23720841aab3
Brian Chebon <brianlchebon@gmail.com>
2024-02-27 13:03:11 +0300

    added var needed

--- main.go
+++ main.go
@@ -3,7 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our Confrence Booking App")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("conferenceName is %T, remainingTickets is %T, conferenceTickets is %T\n", conferenceName, remainingTickets, conferenceTickets)
+
+	fmt.Printf("Welcome to %v Booking Application\n", conferenceName)
+	fmt.Printf("We Have total of %v Tickets and %v are still remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get Your Ticket here to attend")
+
+	var userName string
+	var userTickets int
+	//ask user for their name
+
+	userName = "Brian"
+	userTickets = 50
+	fmt.Printf("User %v Booked %v Tickets\n", userName, userTickets)
 
 }
