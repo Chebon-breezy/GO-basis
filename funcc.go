b92cb5ee16745f6dbd02183eadcb1657b76bafe2
Brian Chebon <brianlchebon@gmail.com>
2024-02-27 10:48:46 +0300

    added functions in Go

---
+++ funcc.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// to run add main class
+func todo() {
+	arr := []int{1, 2, 3, 4}
+	arr2 := []string{"Hi", "my", "name", "is", "Chebon"}
+	fmt.Println(arr, arr2)
+}
